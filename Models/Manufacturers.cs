77f2f9deab074fb37cb633d91be3a944f09e7bfe
Rupinderjeet Singh <200433751@student.georgianc.on.ca>
2020-02-23 22:03:08 -0400

    First Commit

---
+++ Models/Manufacturers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Manufacturers
+    {
+
+        [Key]
+        public int Manufacturer_ID { get; set; }
+        public string Manufacturer_Name { get; set; }
+        public string WebsiteName { get; set; }
+        public DateTime Founded { get; set; }
+
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Products
+    {
+
+        [Key]
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Product_Description { get; set; }
+        public double Price { get; set; }
+
+        [ForeignKey("Manufacturers")]
+        public int Manufacturer_ID { get; set; }
+
+    }
+}
